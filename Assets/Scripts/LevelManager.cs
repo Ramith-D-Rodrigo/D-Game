880c8b20ab0d5624ad53dd4401b57843593e4913
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-09-09 20:47:57 +0530

    create level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("General Attributes")]
+    private int currLevelIndex;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Light directionalLight;
+
+    [Header("Level 2 Attributes")]
+
+    [SerializeField] private BoxCollider lightSwitchingCollider;
+
+    void Awake()
+    {
+        currLevelIndex = SceneManager.GetActiveScene().buildIndex;
+
+        switch(currLevelIndex){
+            case 1:
+                ProcessLevelOneSettings();
+                break;
+
+            case 2:
+                ProcessLevelTwoSettings();
+                break;
+
+            case 3:
+                ProcessLevelThreeSettings();
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    
+    private void ProcessLevelThreeSettings()
+    {
+    }
+
+    private void ProcessLevelTwoSettings()
+    {
+        Rigidbody playerRB = player.GetComponent<Rigidbody>();
+
+        playerRB.constraints = RigidbodyConstraints.None;
+
+        //but freeze rotation on all
+        playerRB.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+
+    private void ProcessLevelOneSettings()
+    {
+        //player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+    }
+}
