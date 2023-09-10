7844899d2410997bbcc9fa1ba9a8be18d953d2ea
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-09-10 19:31:16 +0530

    change player level to script handler

---
+++ Assets/Scripts/Player Scripts/PlayerScriptHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScriptHandler : MonoBehaviour
+{
+    public void StopAllPlayerScripts(){
+        //disable player movement script
+        GetComponent<PlayerMover>().enabled = false;
+        
+        GetComponent<PlayerCollision>().enabled = false;
+
+        GetComponent<PlayerPickable>().enabled = false;
+        
+        GetComponent<PlayerInventory>().enabled = false;
+        //disable player hold object script
+        GetComponent<PlayerHoldObject>().enabled = false;
+
+        //disable player use object script
+        GetComponentInChildren<PlayerUseObject>().enabled = false;
+
+        GetComponent<MovementRecorder>().enabled = false;
+    }
+}
