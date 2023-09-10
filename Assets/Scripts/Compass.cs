74ca9e39557eff108ebb083334b98a8694ae83d2
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-09-10 19:31:56 +0530

    Update Compass.cs

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -7,14 +7,30 @@ public class Compass : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject destination;
     [SerializeField] GameObject pointerPivot;
+
+    [SerializeField] Light pointLight;
+
+    private bool isWorking; //to check whether the compass should work or not
+    public bool IsWorking {get {return isWorking;} set {isWorking = value;} }
     void Start()
     {
-        
+        isWorking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pointerPivot.transform.LookAt(destination.transform);
+        if(isWorking){
+            pointerPivot.transform.LookAt(destination.transform);
+        }
+    }
+
+
+    public void SwitchOnLight(){
+        pointLight.enabled = true;
+    }
+
+    public void SwitchOffLight(){
+        pointLight.enabled = false;
     }
 }
