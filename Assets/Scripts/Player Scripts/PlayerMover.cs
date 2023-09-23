03d66f9066f75828208104dedbe1b85eb86e1a80
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-09-23 16:43:38 +0530

    player movement logic changed

--- Assets/Scripts/Player Scripts/PlayerMover.cs
+++ Assets/Scripts/Player Scripts/PlayerMover.cs
@@ -1,27 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerMover : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float movementSpeed = 1000f;
+    public float movementSpeed = 20f;
     public float rotationSpeed = 250f;
     public float runningSpeedFactor = 2f;
     public float jumpSpeed = 12f;
     public int direction = -1; //player moving direction
-    private readonly float gravity = 9.81f;
-    private float verticalVelocity = 0;
     private Rigidbody rb;
     private Animator playerAnimator;
-    private bool isMoving;
 
     [Header("Grounded Check")]
     [SerializeField] private LayerMask whatIsGround;
-    private float drag;
+    [SerializeField] private float drag;
     private float playerHeight;
     private bool isGrounded;
     [SerializeField] private LevelManager levelManager;
@@ -29,12 +25,17 @@ public class PlayerMover : MonoBehaviour
 
     [SerializeField] private Collider[] bodyCollidersForHeight;
 
+    [SerializeField] private Transform orientation; //use x direction as the forward of the player (positive x is backward, negative x is forward)
+    Vector3 moveDirection = UnityEngine.Vector3.zero;
+    private float horizontalInput;
+    private float verticalInput;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
 
-        isMoving = false;
         isGrounded = true;
 
         playerHeight = 0.0f;
@@ -43,7 +44,7 @@ public class PlayerMover : MonoBehaviour
             playerHeight += collider.bounds.size.y;
         }   
 
-        drag = rb.drag;  
+        rb.drag = drag;
     }
 
     // Update is called once per frame
@@ -57,71 +58,75 @@ public class PlayerMover : MonoBehaviour
                 ProcessLevelTwoMovement();
                 break;
         }
+
+        GetUserInput();
+
+        SpeedControl();
     }
 
     void FixedUpdate()
     {
         MoveForwardBackward();
 
-        StopPlayerMovement();
+        ProcessStopPlayerMovement();
 
         //PlayerJump();
         RotatePlayer();
     }
 
-    /*     void PlayerJump(){
-
-            if(characterController.isGrounded){
-                verticalVelocity = 0;
-                if(Input.GetKeyDown(KeyCode.Space)){
-                    verticalVelocity = jumpSpeed;
-                }
-            }
-            UnityEngine.Vector3 move = UnityEngine.Vector3.up;
-            verticalVelocity -= gravity * Time.deltaTime;
-
-            move.Set(0, verticalVelocity, 0);
-            characterController.Move(move * Time.deltaTime);
-        } */
+    private void GetUserInput(){
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
 
     void MoveForwardBackward(){       
-        if((Input.GetKey(Controls.GoForward) || Input.GetKey(Controls.GoBackward)) && isGrounded){
+        if(isGrounded){
             playerAnimator.SetBool("isWalking", true);
-            float userInput = Input.GetAxis("Vertical");
-            UnityEngine.Vector3 move = new(userInput * Time.fixedDeltaTime, 0f, 0f);
-            
-            if(Input.GetKey(Controls.Running) && userInput > 0){ //only run forward
+
+            Vector3 moveDirection = orientation.right * verticalInput;
+            if(Input.GetKey(Controls.Running) && verticalInput > 0){ //only run forward
                 playerAnimator.SetBool("isRunning", true);
-                move *= runningSpeedFactor;
+                moveDirection *= runningSpeedFactor;
             }
             else{
                 playerAnimator.SetBool("isRunning", false);
             }
 
-            move = transform.TransformDirection(move);
-            move = move * movementSpeed * direction;
-            move.y = rb.velocity.y;
-
-            rb.velocity = move;
-            isMoving = true;
+            rb.AddForce(moveDirection.normalized * direction * movementSpeed * 100f, ForceMode.Force);
         }
     }
 
-    private void StopPlayerMovement(){
-        //player has just released the control keys or has released the keys but the variable is still moving true
-        if(Input.GetKeyUp(Controls.GoForward) || Input.GetKeyUp(Controls.GoBackward) || (!(Input.GetKey(Controls.GoForward) || Input.GetKey(Controls.GoBackward)) && isMoving)){
-            playerAnimator.SetBool("isRunning", false);
-            playerAnimator.SetBool("isWalking", false);
-            rb.velocity = new UnityEngine.Vector3(0.0f, -1.0f, 0.0f);  //stop the player
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-            isMoving = false;
+        if(Input.GetKey(Controls.Running)){
+            if(flatVel.magnitude > movementSpeed * runningSpeedFactor){
+                Vector3 maxVel = flatVel.normalized * movementSpeed * runningSpeedFactor;
+                rb.velocity = new Vector3(maxVel.x, rb.velocity.y, maxVel.z);
+            }
+        }
+        else{
+            if(flatVel.magnitude > movementSpeed){
+                Vector3 maxVel = flatVel.normalized * movementSpeed;
+                rb.velocity = new Vector3(maxVel.x, rb.velocity.y, maxVel.z);
+            }
         }
     }
 
+    private void ProcessStopPlayerMovement(){
+        if(verticalInput == 0){
+            StopMovement();
+        }
+    }
+
+    public void StopMovement(){
+        playerAnimator.SetBool("isRunning", false);
+        playerAnimator.SetBool("isWalking", false);
+    }
 
     void RotatePlayer(){
-        float playerRotation = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * rotationSpeed;
-        UnityEngine.Quaternion deltaRotation = UnityEngine.Quaternion.Euler(0f, playerRotation, 0f);
+        float playerRotation = horizontalInput * Time.deltaTime * rotationSpeed;
+        Quaternion deltaRotation = Quaternion.Euler(0f, playerRotation, 0f);
         rb.MoveRotation(rb.rotation * deltaRotation);
     }
 
@@ -129,7 +134,7 @@ public class PlayerMover : MonoBehaviour
     private void ProcessLevelTwoMovement()
     {
 
-        isGrounded = Physics.Raycast(transform.position, UnityEngine.Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
         if(isGrounded){
             rb.drag = drag;
