4c306402a70ff99709bd9e3ee3f652b5facec895
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-11-18 12:50:40 +0530

    finished mask insertion

--- Assets/Scripts/Obstacles Scripts/Level 2 Obstacles/MaskInsertionHandler.cs
+++ Assets/Scripts/Obstacles Scripts/Level 2 Obstacles/MaskInsertionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class MaskInsertionHandler : MonoBehaviour
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private SpikyWoodenLogs spikyWoodenLogs;
     private HUD hud;
+    private GameObject collidedPlayer;
+    private EnemyMask enemyMask;  //if the collided player has an enemy mask, it will be stored here 
 
     void Start()
     {
@@ -18,7 +21,6 @@ public class MaskInsertionHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     
@@ -32,14 +34,26 @@ public class MaskInsertionHandler : MonoBehaviour
         ProcessPlayerCollision(other);
     }
 
+    private void OnTriggerExit(Collider other) {
+        if(1 << other.gameObject.layer == playerLayer){
+            hud.HideMessage();
+            collidedPlayer = null;
+
+            if(enemyMask){
+                enemyMask.MaskInsertingBox = null;
+                enemyMask = null;
+            }
+        }
+    }
+
 
     private void ProcessPlayerCollision(Collider other){
         if (1 << other.gameObject.layer == playerLayer)
         {
             //get the top most parent of the collided object
-            GameObject collidedObj = other.gameObject.transform.root.gameObject; //top most is the player complete game object
+            collidedPlayer = other.gameObject.transform.root.gameObject; //top most is the player complete game object
 
-            PlayerHoldObject playerHoldObject = collidedObj.GetComponent<PlayerHoldObject>(); //holding object should be the mask placeholder (inside it has the mask)
+            PlayerHoldObject playerHoldObject = collidedPlayer.GetComponent<PlayerHoldObject>(); //holding object should be the mask placeholder (inside it has the mask)
 
             if (!playerHoldObject.CurrHoldingObject)
             {
@@ -50,13 +64,89 @@ public class MaskInsertionHandler : MonoBehaviour
             {
                 Debug.Log("Player has a mask");
 
-                EnemyMask enemyMask = playerHoldObject.CurrHoldingObject.GetComponent<EnemyMask>();
+                enemyMask = playerHoldObject.CurrHoldingObject.GetComponent<EnemyMask>();
                 if (enemyMask.MaskIndex == spikyWoodenLogs.MaskIndex)
                 {
-                    Debug.Log("Player has the correct mask");
                     hud.DisplayGeneralMessage("Press " + Controls.UseObj.ToString() + " to Insert the Mask");
+                    Debug.Log("Player has the correct mask");
+                    enemyMask.MaskInsertingBox = this.gameObject;
+                }
+                else{
+                    Debug.Log("Player has the wrong mask");
                 }
             }
+            else{
+                Debug.Log("Player has no mask");
+            }
+            
+        }
+    }
+
+    public void InsertMask(GameObject mask, int maskIndex){
+        if(maskIndex != spikyWoodenLogs.MaskIndex){
+            return;
+        }
+
+        //hide the message
+        hud.HideMessage();
+
+        //remove the mask from the player
+        collidedPlayer.GetComponent<PlayerHoldObject>().CurrHoldingObject = null;
+        enemyMask.MaskInsertingBox = null;
+        enemyMask = null;
+        collidedPlayer = null;
+
+        //destroy the mask
+        Destroy(mask);
+
+        //play the animation
+        Debug.Log("Inserting the mask");
+
+        Renderer maskRenderer = spikyWoodenLogs.InstantiatedMask.GetComponent<Renderer>();
+
+        StartCoroutine(ChangeColorAndMoveTheLogs(maskRenderer));
+    }
+
+    private IEnumerator ChangeColorAndMoveTheLogs(Renderer renderer)
+    { //direction true -> increase blue, direction false -> decrease blue
+        yield return ChangeColor(renderer);
+
+       MoveTheLogs(renderer);
+    }
+
+    private void MoveTheLogs(Renderer renderer){
+        //remove the material and add the final material
+        renderer.material = null;
+        renderer.material = spikyWoodenLogs.FinalMaskMaterial;
+
+        //move the wooden logs
+        spikyWoodenLogs.CanMove = true;
+    }
+
+    private IEnumerator ChangeColor(Renderer renderer)
+    {
+        yield return ChangeToBlue(renderer);
+
+        yield return ChangeBackFromBlue(renderer);
+
+        yield return ChangeToBlue(renderer);
+    }
+
+    private IEnumerator ChangeToBlue(Renderer renderer){
+        for(int i = 0; i <= 200; i++){
+            Color tempColor = renderer.material.color;
+            Color newColor = new Color(tempColor.r, tempColor.g, i/255.0f, tempColor.a);
+            renderer.material.color = newColor;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator ChangeBackFromBlue(Renderer renderer){
+        for(int i = 200; i >= 0; i--){
+            Color tempColor = renderer.material.color;
+            Color newColor = new Color(tempColor.r, tempColor.g, i/255.0f, tempColor.a);
+            renderer.material.color = newColor;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
