00dc9b665a41a53203af827102497beba6f3f0d9
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-11-18 09:20:13 +0530

    enemy mask collision

--- Assets/Scripts/Obstacles Scripts/Level 2 Obstacles/MaskInsertionHandler.cs
+++ Assets/Scripts/Obstacles Scripts/Level 2 Obstacles/MaskInsertionHandler.cs
@@ -6,9 +6,13 @@ public class MaskInsertionHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private SpikyWoodenLogs spikyWoodenLogs;
+    private HUD hud;
+
     void Start()
     {
-
+        //find the hud component
+        hud = GameObject.FindObjectOfType<HUD>();
     }
 
     // Update is called once per frame
@@ -19,11 +23,40 @@ public class MaskInsertionHandler : MonoBehaviour
 
     
 
-    private void OnTriggerEnter(Collider other) {
-        
-        if(1 << other.gameObject.layer == playerLayer){
-            Debug.Log("player collided with the sphere collider");
-            //other.gameObject.GetComponent<PlayerController>().Die();
+    private void OnTriggerEnter(Collider other)
+    {
+        ProcessPlayerCollision(other);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        ProcessPlayerCollision(other);
+    }
+
+
+    private void ProcessPlayerCollision(Collider other){
+        if (1 << other.gameObject.layer == playerLayer)
+        {
+            //get the top most parent of the collided object
+            GameObject collidedObj = other.gameObject.transform.root.gameObject; //top most is the player complete game object
+
+            PlayerHoldObject playerHoldObject = collidedObj.GetComponent<PlayerHoldObject>(); //holding object should be the mask placeholder (inside it has the mask)
+
+            if (!playerHoldObject.CurrHoldingObject)
+            {
+                return;
+            }
+
+            if (playerHoldObject.CurrHoldingObject.tag == "EnemyMask")
+            {
+                Debug.Log("Player has a mask");
+
+                EnemyMask enemyMask = playerHoldObject.CurrHoldingObject.GetComponent<EnemyMask>();
+                if (enemyMask.MaskIndex == spikyWoodenLogs.MaskIndex)
+                {
+                    Debug.Log("Player has the correct mask");
+                    hud.DisplayGeneralMessage("Press " + Controls.UseObj.ToString() + " to Insert the Mask");
+                }
+            }
         }
     }
 }
